8923ea675dfbf0fa64afbe21d42b686c35f6e7db
Aria Stewart <aredridel@nbtsc.org>
2017-08-06 00:38:12 -0400

    Make it so

---
+++ README.md
@@ -0,0 +1,13 @@
+servepress
+==========
+
+Serve a wordpress instance from within nodejs using PHP-FPM
+
+Requires PHP-FPM to be installed and running on the system already.
+
+Use
+----
+
+```
+PORT=8080 servepress /path/to/wordpress
+```
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const epf = require('express-php-fpm');
+const ehm = require('express-htaccess-middleware');
+const path = require('path');
+const yargs = require('yargs');
+
+const argv = yargs.demand(1).argv;
+
+const app = express();
+
+app.use(ehm({
+  file: path.resolve(argv._[0], '.htaccess'),
+  watch: true
+}))
+
+app.use(epf({
+  documentRoot: path.resolve(argv._[0]),
+  env: {},
+  socketOptions: { port: 9000 }
+}));
+
+app.listen(process.env.PORT || 8080);
